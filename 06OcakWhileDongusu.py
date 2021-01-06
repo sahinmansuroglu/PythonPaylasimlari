f3bcf6705e29bcddf875961c8c6301eaa44d6102
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-01-06 18:13:51 +0300

    Create 06OcakWhileDongusu.py

---
+++ 06OcakWhileDongusu.py
@@ -0,0 +1,26 @@
+# While döngüsü  while komutunun yanında parantez içindeki lart doğru olduğu sürece
+# Altındaki bloğu sürekli çalışıtır.
+# Aşağıdaki uygulamada i=1 olarak verilmiştir ve while ile i<=10 olduğu sürece while döngüsüne
+# bağlı blok çalıştırılarak blok içinde i'nin değeri 1 arttırılarak ekrana 1'den 10 kadar olan sayılar yazdırılmıştır.
+
+i=1
+
+while(i<=10):
+    print(f"{i}")
+    i=i+1
+  
+ 
+# Programın Çıktısı Aşağıdadır.
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+
+
